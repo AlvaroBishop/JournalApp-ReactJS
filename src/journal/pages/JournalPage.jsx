c4081921f30eae05a0e9b8dc690d24b9519a104c
AlvaroBishop <bishopgamma@gmail.com>
2022-09-17 18:09:56 -0500

    layouts, pages, mui

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+import { MailOutline } from '@mui/icons-material'
+export const JournalPage = () => {
+  return (
+    <>
+      <Typography variant='h4' component="h1">JournalPage</Typography>
+    </>
+  )
+}
