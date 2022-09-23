039a8bac4564cf9134302145d325443f9540cd18
AlvaroBishop <bishopgamma@gmail.com>
2022-09-23 17:09:18 -0500

    first commit

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,15 @@
 import { Typography } from '@mui/material'
 import { MailOutline } from '@mui/icons-material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
+import { NoteView } from '../views/NoteView'
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant='h4' component="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti possimus assumenda </Typography>
+      {/* <Typography variant='h4' component="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti possimus assumenda </Typography> */}
+
+      {/* <NothingSelectedView /> */}
+      <NoteView/>
     </JournalLayout>
   )
 }
