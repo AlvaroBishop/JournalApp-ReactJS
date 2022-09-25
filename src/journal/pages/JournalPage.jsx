d865e81376dbc971dc75d09c77d86a42f58348b2
AlvaroBishop <bishopgamma@gmail.com>
2022-09-25 14:57:32 -0500

    completed design

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material'
-import { MailOutline } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
+import { AddOutlined, MailOutline } from '@mui/icons-material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NothingSelectedView } from '../views'
 import { NoteView } from '../views/NoteView'
@@ -8,8 +8,22 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography variant='h4' component="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti possimus assumenda </Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView/>
+      <NothingSelectedView />
+      {/* <NoteView/> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover' : { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
